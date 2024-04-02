7339126960f0c4dc1d7c1ce0af9cee6eb1c6b07c
Vishnu Rajagopal <95849020+vishnu-1821@users.noreply.github.com>
2024-04-02 14:36:38 +0530

    Add files via upload

---
+++ model_test_2.py
@@ -0,0 +1,11 @@
+from transformers import AutoTokenizer, AutoModelForCausalLM
+tokenizer = AutoTokenizer.from_pretrained("Salesforce/codegen-350M-multi")
+model = AutoModelForCausalLM.from_pretrained("Salesforce/codegen-350M-multi")
+
+text = "python code to add 2 numbers"
+input_ids = tokenizer(text, return_tensors="pt").input_ids
+
+generated_ids = model.generate(input_ids, max_length=128)
+print(tokenizer.decode(generated_ids[0], skip_special_tokens=True))
+
+
